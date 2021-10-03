f075e4fb0a9a8bd137a1290985b68a6b40280fed
Lkados <20174890@etud.univ-evry.fr>
2021-10-03 15:58:51 +0200

    git ignore

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://www.mohamedk.fr/CanImoApi/public/',
+    baseURL: 'https://canardemo.herokuapp.com',
 });
 
 axiosInstance.defaults.withCredentials = true;
